0217f45d0a1ebe5ea930b068aebbd4c4458d2742
Pranjul Tiwari <pranjultiwari7800@gmail.com>
2023-05-11 02:00:58 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let score0 = document.getElementById('score--0');
 let score1 = document.getElementById('score--1');
 score0.textContent = 0;
 score1.textContent = 0;
+let dice = document.querySelector('.dice');
+dice.classList.add('hidden');
