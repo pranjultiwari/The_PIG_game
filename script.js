fa8813ff1345ee84e1c55bbfd6c00d2a27ef421f
Pranjul Tiwari <pranjultiwari7800@gmail.com>
2023-05-30 21:18:03 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 let score0 = document.getElementById('score--0');
 let score1 = document.getElementById('score--1');
+let dice = document.querySelector('.dice');
+let rollDice = document.querySelector('.btn--roll');
+let current_0= document.getElementById('current--0');
+
+let current = 0;
+
 score0.textContent = 0;
 score1.textContent = 0;
-let dice = document.querySelector('.dice');
 dice.classList.add('hidden');
+rollDice.addEventListener('click', function()
+{
+    let diceNumber = Math.trunc(Math.random()*6)+1;
+    console.log(diceNumber)
+
+    dice.classList.remove('hidden');
+    dice.src = `images/dice-${diceNumber}.png`;
+
+    if(diceNumber != 1)
+    {
+    current += diceNumber;
+    current_0.textContent = current;
+    }
+    
+})
